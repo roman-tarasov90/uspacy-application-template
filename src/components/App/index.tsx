ae7d50ebe4fdc08679a924bde8930b031bb6ca3b
Roman Tarasov <rtarasov1k90@gmail.com>
2024-03-15 13:37:50 +0200

    Greet

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -18,6 +18,7 @@ const App: React.FC = () => {
 				color: (theme) => theme.palette.primary.main,
 			}}
 		>
+			<p>ПРИВЕТ И ТЕБЕ</p>
 			{t('helloWorld')} | {profile?.firstName} {profile?.lastName}
 		</Box>
 	);
